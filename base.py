a6122762c4393cacb1ea9eb4283eea04d6c64f24
Ofer Sadan <ofersadan85@gmail.com>
2022-08-07 11:11:08 +0300

    split RotatingThing from Thing

--- base.py
+++ base.py
@@ -4,26 +4,26 @@ from typing import Sequence
 
 import numpy as np
 
-from vectors import Vector2d, normalize_angle
+from vectors import Vector2d, clockwise_angle, normalize_angle
 
 
 @dataclass
 class Thing:  # Currently only used in 2D
     """A thing is any object with changing position and mass"""
 
-    mass: float = 1.0
     position: Sequence = field(default_factory=Vector2d)
     velocity: Sequence = field(default_factory=Vector2d)
+    mass: float = 1.0
     lock_x: bool = False
     lock_y: bool = False
-    uid: uuid.UUID = field(default_factory=uuid.uuid4)
+    uid: uuid.UUID = field(default_factory=uuid.uuid4, init=False)
 
     def __post_init__(self):
-        self.mass = abs(self.mass)
         if not isinstance(self.position, Vector2d):
             self.position = Vector2d(*self.position)
         if not isinstance(self.velocity, Vector2d):
             self.velocity = Vector2d(*self.velocity)
+        self.mass = abs(self.mass)
 
     def __hash__(self) -> int:
         return hash(self.uid)
@@ -37,8 +37,9 @@ class Thing:  # Currently only used in 2D
 
     def update(self):
         """Update position"""
-        self.velocity *= [self.lock_x, self.lock_y]
-        self.position += self.velocity
+        lock = [not(self.lock_x), not(self.lock_y)]
+        self.velocity = self.velocity * lock
+        self.position = self.position + self.velocity
 
     def bounce(self, axis: int = 0, ratio: float = 1.0) -> None:
         """
@@ -46,6 +47,25 @@ class Thing:  # Currently only used in 2D
         i.e. if ratio is 0.99, the velocity will be reversed and reduced by 1%
         """
         self.velocity[axis] = -self.velocity[axis] * ratio
+
+    def copy(self) -> "Thing":
+        """Create a copy of this thing"""
+        return Thing(
+            position=self.position.copy(),
+            velocity=self.velocity.copy(),
+            mass=self.mass,
+            lock_x=self.lock_x,
+            lock_y=self.lock_y,
+        )
+
+    @classmethod
+    def random(cls) -> "Thing":
+        """Create a random thing"""
+        return Thing(
+            position=Vector2d.random(),
+            velocity=Vector2d.random(),
+            mass=np.random.rand(),
+        )
 
 
 @dataclass
@@ -60,6 +80,12 @@ class RotatingThing(Thing):
         super().__post_init__()
         self.heading = normalize_angle(self.heading)
 
+    def __hash__(self) -> int:
+        return hash(self.uid)
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Thing) and self.uid == other.uid
+
     def update(self):
         """Update position and heading"""
         super().update()
@@ -67,3 +93,27 @@ class RotatingThing(Thing):
             self.angular_velocity = 0.0
         self.heading += self.angular_velocity
         self.heading = normalize_angle(self.heading)
+
+    def copy(self) -> "RotatingThing":
+        """Create a copy of this thing"""
+        return RotatingThing(
+            position=self.position.copy(),
+            velocity=self.velocity.copy(),
+            mass=self.mass,
+            heading=self.heading,
+            angular_velocity=self.angular_velocity,
+            lock_x=self.lock_x,
+            lock_y=self.lock_y,
+            lock_heading=self.lock_heading,
+        )
+
+    @classmethod
+    def random(cls) -> "RotatingThing":
+        """Create a random RotatingThing"""
+        return RotatingThing(
+            position=Vector2d.random(),
+            velocity=Vector2d.random(),
+            mass=np.random.rand(),
+            heading=(np.random.rand() * 2 * np.pi),
+            angular_velocity=np.random.rand(),
+        )
